213f6c5605124eb7fa5b600c610d2794c55b1fe6
gtgopinath5 <gtgopinath5@gmail.com>
2024-02-20 11:58:14 +0530

    commit

---
+++ dom.js
@@ -0,0 +1,11 @@
+function foo(){
+var ele =document.getElementById("email").value;
+console.log(ele);
+var f =document.getElementById("firstname").value;
+console.log(f);
+var m =document.getElementById("middlename").value;
+console.log(m);
+var l =document.getElementById("lastname").value;
+console.log(l);
+
+}
